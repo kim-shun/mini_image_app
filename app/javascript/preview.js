b64658126a301e2c6b355b9b73ba59b553dceda8
kim-shun <kimura1417080@gmail.com>
2021-06-17 10:13:04 +0900

    画像をブラウザに表示させる

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,5 +3,13 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('message_image').addEventListener('change', function(e){
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement('div'); //画像を表示するためのdiv要素を生成
+    const blobImage = document.createElement('img'); //表示する画像を生成
+    blobImage.setAttribute('src', blob);
+
+    //生成したHTMLの要素をブラウザに表示、指定した親要素の中に要素を追加するメソッド
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
   });
 });
