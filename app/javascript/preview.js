09bd67d09a9634c3b3ba2e07afbd943a89457c6a
kim-shun <kimura1417080@gmail.com>
2021-06-20 14:55:29 +0900

    ファイル選択ボタンの追加

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function(){
       const blobImage = document.createElement('img'); //表示する画像を生成
       blobImage.setAttribute('src', blob);
 
+      const inputHTML = document.createElement('input') //ファイル選択ボタンを生成
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
+
+      //↑<input id="message_image_[番号]" name="message[images][]" type="file">を生成
+
       //生成したHTMLの要素をブラウザに表示、指定した親要素の中に要素を追加するメソッド
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
